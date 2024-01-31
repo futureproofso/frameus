058e25566a9c6e14173f84064e04d9c447e33659
sticky <sticky@fp>
2024-01-30 21:21:58 -0500

    fix: don't use js in imports

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -20,9 +20,9 @@ import {
 import {
   useRoomConnection,
 } from "@whereby.com/browser-sdk/react";
-import { FadeBlack } from "../_components/FadeOut/FadeOut.js";
-import { VideoOther } from "../_components/Video/VideoOther.js";
-import { VideoSelf } from "../_components/Video/VideoSelf.js";
+import { FadeBlack } from "../_components/FadeOut/FadeOut";
+import { VideoOther } from "../_components/Video/VideoOther";
+import { VideoSelf } from "../_components/Video/VideoSelf";
 
 const defaultButtonProps: Partial<ButtonProps> = {
   color: "fl-secondary",
